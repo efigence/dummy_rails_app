ced78d741fab5fcbddeec5b25e7462fc41a1aeab
Marcin Kalita <rubyconvict@gmail.com>
2017-07-14 13:22:34 +0200

    Cleanup info

--- config/deploy/personal_production.rb
+++ config/deploy/personal_production.rb
@@ -2,35 +2,5 @@ if File.exist?(File.join(File.dirname(__FILE__), 'personal_production.local.rb')
   load 'config/deploy/personal_production.local.rb'
 end
 
-# after 'deploy:update_code', 'isitworking:touch'
-# after 'deploy', 'mark_revision'
+# Comment this out if using HA setup
 after 'deploy:restart', 'maintenance:off'
-
-# # is it working
-# namespace :isitworking do
-#   task :touch, roles: :web, except: { no_release: true } do
-#     cmd = %( sudo -iu #{process_owner_user} touch #{latest_release}/tmp/isItWorking.txt )
-#     run cmd
-#   end
-# TODO: rewrite
-# on roles :all do
-#   within fetch(:latest_release_directory) do
-#     with rails_env: fetch(:rails_env) do
-#       execute :rake, 'assets:precompile'
-#     end
-#   end
-# end
-
-# end
-
-# task :mark_revision do
-#   log = "#{shared_path}/versions"
-#   cmd = <<-CMD
-#     sudo -iu #{process_owner_user} touch #{log} &&
-#     sudo -iu #{process_owner_user} chmod g+w -R  #{log} &&
-#     sudo -iu #{process_owner_user} chgrp -R mongrel #{log} &&
-#     echo "#{DateTime.now} #{latest_revision}<br />" >> #{log} &&
-#     sudo -iu #{process_owner_user} ln -s #{log} #{current_release}/public/versions.html
-#   CMD
-#   run cmd.gsub(/\r?\n/, '').gsub(/\s+/, ' ')
-# end
