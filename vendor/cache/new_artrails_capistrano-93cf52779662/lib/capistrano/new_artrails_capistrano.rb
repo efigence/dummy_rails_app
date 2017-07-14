722fdb3bb65ba6bc2b5409d1791bfadfb17751ba
Marcin Kalita <rubyconvict@gmail.com>
2017-07-14 11:17:30 +0200

    Cleanup gem cache

--- vendor/cache/new_artrails_capistrano-93cf52779662/lib/capistrano/new_artrails_capistrano.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-load File.expand_path('../tasks/new_artrails_capistrano.rake', __FILE__)
--- vendor/cache/new_artrails_capistrano-93cf52779662/lib/capistrano/new_artrails_capistrano/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Capistrano
-  module NewArtrailsCapistrano
-    VERSION = '0.0.1'
-  end
-end
